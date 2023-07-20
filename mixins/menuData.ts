4cbbf98b4f9ef7c5a883e73eb78c7edfb06e4a44
gstrainovic <g.strainovic@gmail.com>
2023-07-20 22:38:50 +0200

    fix mobile menu

--- mixins/menuData.ts
+++ mixins/menuData.ts
@@ -4,21 +4,25 @@ export default {
       menuData: [
         {
           id: 1,
+          hasDropdown: true, // needed for mobile menu
           title: "Services",
-          link: "service-details",
+          link: "/service-details",
         },
         {
           id: 2,
+          hasDropdown: true, // needed for mobile menu
           title: "Events",
           link: "event-details",
         },
         {
           id: 3,
+          hasDropdown: true, // needed for mobile menu
           title: "Blog",
           link: "blog-details",
         },
         {
           id: 4,
+          hasDropdown: true, // needed for mobile menu
           title: "Company",
           link: "/company",
         },
