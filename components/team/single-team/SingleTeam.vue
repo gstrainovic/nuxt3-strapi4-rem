9887e31078607cfd224d06b6e607189641b9c4f9
gstrainovic <g.strainovic@gmail.com>
2023-07-21 00:17:32 +0200

    fix email on team member

--- components/team/single-team/SingleTeam.vue
+++ components/team/single-team/SingleTeam.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="`team__item ${team_p?'mb-55':''}`">
+  <div class="team__item">
     <div class="team__thumb w-img fix transition-3">
       <div class="tp-thumb-overlay wow"></div>
       <nuxt-link :to="localePath('/team-details') + '/' + id">
@@ -8,19 +8,7 @@
       <div class="team__action">
         <ul>
           <li>
-            <a href="mailto:team@harry.com"><i class="far fa-envelope"></i></a>
-          </li>
-          <li>
-            <!-- <a href="#"><i class="far fa-share-alt"></i></a> -->
-            <!-- <div class="team__social">
-              <ul>
-                <li v-for="(l, i) in item.social_links" :key="i">
-                  <a :href="l.link" :target="l.target">
-                    <i :class="l.icon"></i>
-                  </a>
-                </li>
-              </ul>
-            </div> -->
+            <a :href="'mailto:' + item.email"><i class="far fa-envelope"></i></a>
           </li>
         </ul>
       </div>
@@ -38,18 +26,14 @@
 import { Author } from 'types/author';
 const localePath = useLocalePath();
 
-const { item, team_p } = defineProps({
+const { item } = defineProps({
   id: {
-    type: String,
+    type: Number,
     required: true,
   },
   item: {
     type: Object as PropType<Author>,
     required: true,
-  },
-  team_p: {
-    type: Boolean,
-    default: false,
   },
 })
 
